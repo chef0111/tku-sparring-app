64d04a90b218bbf76c888e3c1d5a04d2330c0ef4
chef0111 <letrangiabao14@gmail.com>
2026-02-06 13:57:27 +0700

    feat(ui): update match result screen

--- src/components/modules/app/match-result/index.tsx
+++ src/components/modules/app/match-result/index.tsx
@@ -9,6 +9,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { Ring } from '@/components/ui/ring';
+import { Card } from '@/components/ui/card';
 
 interface ResultDialogProps {
   winner: Player | null;
@@ -37,61 +38,59 @@ export const ResultDialog = ({
       closeButtonClassName="scale-200! top-6 right-6 no-focus"
     >
       <DialogHeader>
-        <DialogTitle className="text-center text-8xl font-bold text-nowrap select-none">
+        <DialogTitle className="text-center text-8xl font-bold tracking-wide text-nowrap select-none">
           MATCH RESULT
         </DialogTitle>
       </DialogHeader>
 
       {/* Result */}
       <div className="flex h-44 w-full max-w-7xl items-stretch gap-4">
-        <div className="winner-label w-[64%] bg-yellow-200 text-7xl text-black">
+        <Card className="winner-label tex-foreground w-[64%] border-yellow-300 bg-yellow-200/30 text-7xl ring-yellow-600">
           WINNER
-        </div>
-        <div className="winner-label w-[18%] bg-blue-600 text-8xl text-white">
+        </Card>
+        <Card className="winner-label w-[18%] border-blue-500 bg-[#233448] text-8xl text-blue-500 ring-indigo-900">
           {blueWon}
-        </div>
-        <div className="winner-label w-[18%] bg-red-600 text-8xl text-white">
+        </Card>
+        <Card className="winner-label border-destructive text-destructive w-[18%] bg-[#46282a] text-8xl ring-red-900">
           {redWon}
-        </div>
+        </Card>
       </div>
 
       {/* Winner info */}
       <div className="flex h-44 w-full max-w-7xl items-stretch gap-4">
         <Avatar
           className={cn(
-            'h-full w-[26%] rounded-sm bg-white/10 after:border-none',
-            winner === 'red' ? 'bg-red-600/10' : 'bg-blue-600/10'
+            'h-full w-[26%] rounded-xl bg-white/10 after:border-none',
+            winner === 'red' ? 'bg-red-600/20' : 'bg-blue-600/20'
           )}
         >
-          <Ring className="rounded-sm ring-3" />
+          <Ring className="rounded-xl ring-5" />
           <AvatarImage
             src={winnerAvatar}
             alt={winnerName}
-            className="h-full w-full rounded-none object-contain"
+            className="h-full w-full rounded-xl object-contain"
           />
           <AvatarFallback className="text-4xl font-bold">
             {winner === 'red' ? 'A' : 'B'}
           </AvatarFallback>
         </Avatar>
 
-        <div
+        <Card
           className={cn(
             'winner-label w-[74%] text-7xl text-white',
-            winner === 'red' ? 'bg-red-600' : 'bg-blue-600'
+            winner === 'red'
+              ? 'border-destructive bg-[#46282a] ring-red-900'
+              : 'border-blue-600 bg-[#233448] ring-indigo-900'
           )}
         >
           {winnerName}
-        </div>
+        </Card>
       </div>
 
       {/* Actions */}
       <DialogFooter className="flex w-full max-w-7xl items-center justify-center! gap-6">
-        <Button className="action-btn bg-destructive hover:bg-red-500">
-          CANCEL RESULT
-        </Button>
-        <Button className="action-btn bg-green-600 hover:bg-green-700">
-          ACCEPT RESULT
-        </Button>
+        <Button className="action-btn action-btn-cancel">CANCEL RESULT</Button>
+        <Button className="action-btn action-btn-accept">ACCEPT RESULT</Button>
       </DialogFooter>
     </DialogContent>
   );
